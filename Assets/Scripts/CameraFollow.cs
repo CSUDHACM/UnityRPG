0a86421ebc259be4eee2bc1d1152b18683d61d49
Kevin Ramirez <kevinramirez266@yahoo.com>
2018-09-12 10:57:19 -0700

    Added Base for RPG
    -Added "Player" complete with movement script and basic animations.
    -Added "Home" Tilemap, complete with 4 layers defining details independant of each other.
    -Added "CameraFollow" script which enables camera to follow player.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Vector3 offset;
+    public Transform lookAt;
+
+	void LateUpdate() {
+        transform.position = lookAt.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed;
+    private Animator anim;
+    private float horizontalDir, verticalDir, lastX, lastY;
+    private bool isMoving;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update () {
+        horizontalDir = Input.GetAxisRaw("Horizontal");
+        verticalDir = Input.GetAxisRaw("Vertical");
+        isMoving = false;
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(horizontalDir * moveSpeed * Time.deltaTime, 0f, 0f));
+            isMoving = true;
+            lastX = horizontalDir;
+            lastY = 0f;
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, verticalDir * moveSpeed * Time.deltaTime, 0f));
+            isMoving = true;
+            lastX = 0f;
+            lastY = verticalDir;
+        }
+
+        anim.SetFloat("MoveX", horizontalDir);
+        anim.SetFloat("MoveY", verticalDir);
+        anim.SetBool("IsMoving", isMoving);
+        anim.SetFloat("LastMoveX", lastX);
+        anim.SetFloat("LastMoveY", lastY);
+    }
+}
