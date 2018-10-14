dfddd9d3cbecaaa55c76a583c7b0c9a6f2efefe8
Kevin Ramirez <kevinramirez266@yahoo.com>
2018-10-13 22:26:36 -0700

    General File Reallocation
    -Moved all spritesheet files and palletes by name to their respective folders under Textures/Spritesheets
    -Imported textures to create characters, includes display weapons, clothing, hair and armor.
    -Adjusted CameraFollow and PlayerController script to prevent unloading and reloading the character and camera when moving from scene to scene.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
-    public Vector3 offset;
-    public Transform lookAt;
+    public Vector3 offset;  //Variable offset declared as a Vector3 Object, stores X, Y, and Z coordinates.
+    public Transform lookAt;    //Variable lookAt declared as Transform object, 
+                                    //behaves as coordinates to move the camera to the player's position.
 
-	void LateUpdate() {
+    private static bool cameraExists;   //Variable to determine whether the camera already exists in a scene, if so do not create a new
+                                            //camera whenever entering another scene.
+
+    private void Start()
+    {
+        if (!cameraExists)
+        {
+            cameraExists = true;    //Sets player exists to true to prevent duplicating player.
+            DontDestroyOnLoad(transform.gameObject);    //Prevents destroying the current player object whenever moving to another scene.
+        }
+        else
+        {
+            Destroy(transform.gameObject);  //Destroy player when unity duplicates player upon moving to an ew scene
+        }
+    }
+
+    void LateUpdate() {
         transform.position = lookAt.position + offset;
 	}
 }
